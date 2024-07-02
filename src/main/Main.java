8870a2fa7bf279aa67df1d36828d40e77ba83ace
Ruslanych <ruslanychab@gmail.com>
2024-07-02 18:00:35 +0200

    [main]: test commit №1

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		// _Writer_
+		// _Date_
+		//
+		// package | main
+		// class   | Main
+		// method  | main
+		//   Executes a script from main.calculation,
+		//   where the solution is contained.
+		System.out.println("\"main.Main().main()\" execution.");
+	}
+}
